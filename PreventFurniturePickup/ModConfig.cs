bf53b905c45984526067ff27c51a2219b3e9fb71
elizabethcd <55162529+elizabethcd@users.noreply.github.com>
2021-12-18 14:03:09 -0500

    Adding config options

---
+++ PreventFurniturePickup/ModConfig.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace PreventFurniturePickup
+{
+    public class ModConfig
+    {
+        // Add an option for each furniture type
+        public bool CanPickUpBed { get; set; }
+        public bool CanPickUpChair { get; set; }
+        public bool CanPickUpTable { get; set; }
+        public bool CanPickUpDresser { get; set; }
+        public bool CanPickUpDecoration { get; set; }
+        public bool CanPickUpLamp { get; set; }
+        public bool CanPickUpRug { get; set; }
+        public bool CanPickUpWindow { get; set; }
+        public bool CanPickUpFireplace { get; set; }
+        public bool CanPickUpTorch { get; set; }
+        public bool CanPickUpSconce { get; set; }
+
+        public ModConfig()
+        {
+            // By default, allow all furniture types to be picked up
+            this.CanPickUpBed = true;
+            this.CanPickUpChair = true;
+            this.CanPickUpTable = true;
+            this.CanPickUpDresser = true;
+            this.CanPickUpDecoration = true;
+            this.CanPickUpLamp = true;
+            this.CanPickUpRug = true;
+            this.CanPickUpWindow = true;
+            this.CanPickUpFireplace = true;
+            this.CanPickUpTorch = true;
+            this.CanPickUpSconce = true;
+        }
+    }
+}
